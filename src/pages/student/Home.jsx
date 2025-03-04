3dd9b28781ea94e6342b69692a1b638d60f9073f
Neha Kumari <nehakashyap1011@gmail.com>
2025-03-04 07:32:02 +0530

    Added companies components

--- src/pages/student/Home.jsx
+++ src/pages/student/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../../components/student/Hero'
+import Companies from '../../components/student/Companies'
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center w-full md:pt-36 pt-20 px-7 md:px-0 space-y-7 text-center bg-gradient-to-b from-cyan-100/70">
+    <div className="flex flex-col items-center justify-center w-full md:pt-32 pt-20 px-7 md:px-0 space-y-7 text-center bg-gradient-to-b from-cyan-100/70">
       <Hero/>
+      <Companies/>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,9 @@ export default {
     fontSize :{
       'course-details-heading-small': ['26px', '36px'],
      
-      'course-details-heading-large' : ['36px', '44px'],
+      'course-details-heading-large' : ['30px', '36px'],
       'home-heading-small':['28px', '36px'],
-      'home-heading-large':['40px', '56px'],
+      'home-heading-large':['32px', '48px'],
       'default' : ['16px', '21px'],
     }
 
