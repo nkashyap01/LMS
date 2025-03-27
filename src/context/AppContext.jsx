9fa3e0490f5a62a0d75a6da5d56ccdffec4879fc
Neha Kumari <nehakashyap1011@gmail.com>
2025-03-27 12:49:14 +0530

    completed testimonials and call to action section in home page

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,14 +1,31 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 export const AppContext = createContext()
 export const AppContextProvider = (props) =>{
 
     const currency = import.meta.env.VITE_CURRENCY
 
     const [allCourses, setAllCourses] = useState([])
+    const [isEducator, setIsEducator] = useState(true)
+    const navigate = useNavigate();
 
     const fetchAllCourses = async () => {
         setAllCourses(dummyCourses)
+    }
+
+    // function to caluculate average rating
+
+    const calculateRating = (course) => {
+        if(course.courseRatings.length===0){
+            return 0;
+        }
+        let totalRating = 0;
+        course.courseRatings.forEach(rating => {
+            totalRating += rating.rating
+        })
+
+        return totalRating / course.courseRatings.length
     }
 
      useEffect(()=>{
@@ -16,7 +33,7 @@ export const AppContextProvider = (props) =>{
      },[])
 
     const value={
-        currency, allCourses
+        currency, allCourses, navigate, calculateRating, isEducator, setIsEducator
 
     }
     return(
--- src/pages/student/Home.jsx
+++ src/pages/student/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Hero from '../../components/student/Hero'
 import Companies from '../../components/student/Companies'
 import CoursesSection from '../../components/student/CoursesSection'
+import TestimonialsSection from '../../components/student/TestimonialsSection'
+import CallToAction from '../../components/student/CallToAction'
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <Hero/>
       <Companies/>
       <CoursesSection/>
+      <TestimonialsSection/>
+      <CallToAction/>
     </div>
   )
 }
