ff6d991c66259d403fb31499b20d9f314179cee8
Neha Kumari <nehakashyap1011@gmail.com>
2025-05-11 19:24:12 +0530

    Updated Search button UI

--- src/components/student/Hero.jsx
+++ src/components/student/Hero.jsx
@@ -4,13 +4,13 @@ import SearchBar from './SearchBar'
 
 const Hero = () => {
   return (
-    <div>
-      <h1 className="md:text-home-heading-large text-home-heading-small relative font-bold text-gray-800 max-w-3xl mx-auto">Empower your future with the 
+    <div >
+      <h1 className="md:text-home-heading-large text-home-heading-small relative font-bold text-gray-800 max-w-3xl mx-auto ">Empower your future with the 
       courses designed to <span className="text-[#2563EB]
 ">fit your choice.</span>
-<img src={assets.sketch} alt="sketch" className="md:block hidden absolute -bottom-7 right-0" />
+<img src={assets.sketch} alt="sketch" className="md:block hidden absolute -bottom-7 right-0 z-0" />
 </h1>
-<p className="md:block hidden text-gray-500 max-w-2xl mx-auto"> We bring together world-class instructors, interactive content, and a supportive
+<p className="md:block hidden text-gray-500 max-w-2xl mx-auto relative z-10"> We bring together world-class instructors, interactive content, and a supportive
 community to help you achieve your personal and professional goals. </p>
 
 <p className="md:hidden text-gray-500 max-w-sm mx-auto"> We bring together world-class instructors, interactive content, and a supportive
--- src/components/student/SearchBar.jsx
+++ src/components/student/SearchBar.jsx
@@ -18,7 +18,7 @@ const SearchBar = ({data}) => {
       <form onSubmit= {onSearchHandler} className="max-w-3xl w-full md:h-14 h-12 flex items-center bg-white border border-gray-500/20 rounded">
         <img src={assets.search_icon} alt="search_icon" className="md:w-auto w-10 px-3"/>
         <input onChange = {e => setInput(e.target.value)} value={input} type="text" placeholder='search for courses' className="w-full h-full outline-none text-gray-500/80"/>
-        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold md:px-10 px-7 md:py-3 py-2"> Search </button>
+        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold md:px-10 px-7 md:py-4 py-2 rounded-md mr-0"> Search </button>
       </form>
     </div>
   )
