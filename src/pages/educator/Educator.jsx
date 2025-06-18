ea900d07dc5056828dfed6ae785c1faa66a5bcf8
Neha Kumari <nehakashyap1011@gmail.com>
2025-06-18 23:39:01 +0530

    working on Educator Dashboard

--- src/pages/educator/Educator.jsx
+++ src/pages/educator/Educator.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
+import Navbar from '../../components/educator/Navbar';
+import Sidebar from '../../components/educator/Sidebar';
 
 const Educator = () => {
   return (
-    <div className="bg-white py-16 px-4">
-      <div className="max-w-4xl mx-auto text-center">
-        <h1 className="md:text-5xl text-3xl font-bold text-gray-800 mb-6">
-          Meet Our <span className="text-[#2563EB]">Expert Educators</span>
-        </h1>
-        <p className="text-gray-500 max-w-2xl mx-auto">
-          Our instructors are experienced professionals and passionate teachers
-          from across industries, bringing real-world expertise into every course.
-        </p>
+    <div className="text-default min-h-screen text-white">
+      <Navbar  />
+      <div className="flex">
+        <Sidebar/>
+        <div className="flex-1">
+       { <Outlet/> }
+       </div>
       </div>
     </div>
   )
