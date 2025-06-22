7f41f07512d0f7cb6eb9eac62e63772eb576468a
Neha Kumari <nehakashyap1011@gmail.com>
2025-06-22 18:07:34 +0530

    Completed Dashboard of Educator Page

--- src/App.jsx
+++ src/App.jsx
@@ -26,8 +26,8 @@ const App = () => {
         <Route path="my-enrollments" element={<MyEnrollments />} />
         <Route path="player/:courseId" element={<Player />} />
         <Route path="loading/:path" element={<Loading />} />
-        <Route path="educator" element={<Educator />}>
-          <Route path="dashboard" element={<Dashboard />} />
+        <Route path="/educator" element={<Educator />}>
+          <Route path="/educator" element={<Dashboard />} />
           <Route path="add-course" element={<AddCourse />} />
           <Route path="my-course" element={<MyCourses />} />
           <Route path="students-enrolled" element={<StudentsEnrolled />} />
