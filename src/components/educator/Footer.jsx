883e1c4ec27bd41d5e99e392cb37a3bf1a035e45
Neha Kumari <nehakashyap1011@gmail.com>
2025-06-22 14:58:40 +0530

    implemented  Navbar, Sidebar, and Footer components for Educator section

--- src/components/educator/Footer.jsx
+++ src/components/educator/Footer.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+   <footer className="flex md:flex-row flex-col-reverse items-center justify-between text-left w-full px-8 border-t">
+{/* left column */}
+<div className="flex items-center gap-4">
+  <img className="hidden md:block w-20" src={assets.logo} alt="logo" />
+  <div className="hidden md:block h-7 w-px bg-gray-500/60"></div>
+  <p className="text-black"> Copyright 2025 @ Neha. All Right Reserved. </p>
+</div>
+{/* Right Column */}
+<div className="flex items-center gap-3 max-md:mt-4">
+  <a href="#">
+    <img src={assets.facebook_icon} alt="facebook_icon" />
+  </a>
+  <a href="#">
+    <img src={assets.twitter_icon} alt="twitter_icon" />
+  </a>
+  <a href="#">
+    <img src={assets.instagram_icon} alt="instagram_icon" />  
+  </a>
+</div>
+   </footer>
   )
 }
 
